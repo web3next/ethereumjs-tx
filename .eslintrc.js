46e536e706d65f0685c739c40f8170c3d14c5950
zigen <hrlclb@gmail.com>
2018-06-13 23:47:02 +0900

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "canonical",
+    "canonical/ava",
+    "canonical/flowtype",
+    "canonical/jest",
+    "canonical/mocha"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "promise"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "prefer-const": [
+      "error"
+    ]
+  }
+};
